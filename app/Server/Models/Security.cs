80708271035864ce0b97779b0bf422e29ac32cfe
Thomas Claiborne <thomas.claiborne123@gmail.com>
2026-05-14 08:11:34 -0400

    Security model + enums

---
+++ app/Server/Models/Security.cs
@@ -0,0 +1,10 @@
+namespace Server.Models;
+
+public class Security
+{
+    public string Ticker { get; set; } = null!;
+    public string CompanyName { get; set; } = null!;
+    public AssetClass AssetClass { get; set; }
+    public decimal? LastPrice { get; set; }
+    public DateTime? PriceFetchedAt { get; set; }
+}
