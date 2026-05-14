b4ceb3ab01a732f43fbe0620e903124bd8089e32
Thomas Claiborne <thomas.claiborne123@gmail.com>
2026-05-14 09:33:46 -0400

    SecurityRepository + tests

---
+++ app/Server/Data/ISecurityRepository.cs
@@ -0,0 +1,10 @@
+using Server.Models;
+
+namespace Server.Data;
+
+public interface ISecurityRepository
+{
+    Task<List<Security>> GetAll();
+    Task<Security?> GetByTicker(string ticker);
+    Task<bool> UpdatePrice(string ticker, decimal price);
+}
